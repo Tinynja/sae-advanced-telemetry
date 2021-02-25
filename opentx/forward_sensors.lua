13474ef73cca5db6ab661707b3f67b9a6eafc443
Tinynja <rantamplan1911@gmail.com>
2021-02-25 01:51:59 -0500

    opentx script done

--- opentx/forward_sensors.lua
+++ opentx/forward_sensors.lua
@@ -1,11 +1,22 @@
 local function init()
 	-- called once when model is loaded
-
+	sources = {}
+	local file = io.open('sources.txt', 'r')
+	local src = file:read('l')
+	while src ~= nil do
+		table.insert(sources, src)
+		src = file:read('l')
+	end
 end
 
 local function background()
 	-- called periodically
-	local x = 2
+	packet = ''
+	for i,v in ipairs(sources) do
+		packet = packet .. sources[i] .. '\x03' .. getValue(sources[i]) ..'\x03'
+	end
+	packet = packet .. '\x04'
+	serialWrite(packet)
 end
 
 return {init=init, background=background}

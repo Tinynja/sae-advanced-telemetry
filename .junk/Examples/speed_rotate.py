e76aaac632a076d8135b90821743ab869c29a837
Tinynja <rantamplan1911@gmail.com>
2021-03-01 23:55:31 -0500

    rotate_example

--- .junk/Examples/speed_rotate.py
+++ .junk/Examples/speed_rotate.py
@@ -9,13 +9,14 @@ QGridLayout(main_widget)
 
 original = QPixmap('Speeds.png')
 
-top = 0
+angle = 0
 height = 80
 
 def update_img(adj=0):
-	global top
-	top = max(0, min(top+adj, original.height()-height))
-	img.setPixmap(original.copy(QRect(0, top, original.width(), height)))
+	global angle
+	angle += adj
+	transform = QTransform().rotate(angle)
+	img.setPixmap(original.transformed(transform, Qt.SmoothTransformation))
 
 img = QLabel()
 update_img()
@@ -32,13 +33,13 @@ down.setIcon(up.style().standardIcon(QStyle.SP_TitleBarUnshadeButton))
 main_widget.layout().addWidget(img, 0, 0, 4, 1)
 main_widget.layout().addWidget(up, 1, 1)
 main_widget.layout().addWidget(down, 2, 1)
-
-main_widget.layout().setRowStretch(0, 1)
 main_widget.layout().setRowStretch(3, 1)
+main_widget.layout().setRowStretch(0, 1)
 main_widget.layout().setColumnStretch(0, 1)
 
 up.clicked.connect(lambda: update_img(-10))
 down.clicked.connect(lambda: update_img(10))
+
 
 main_widget.show()
 app.exec()

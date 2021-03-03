c371b9e9b88cb6bf3912cda2ec77c40fe8471e1f
lioneloued17 <lioneloued17@gmail.com>
2021-03-03 18:10:05 -0500

    essai

--- Fred/layoutLionel.py
+++ Fred/layoutLionel.py
@@ -10,23 +10,58 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        hbox = QHBoxLayout(self)
+
+        topleft = QFrame(self)
+        topleft.setFrameShape(QFrame.StyledPanel)
+
+        topright = QFrame(self)
+        topright.setFrameShape(QFrame.StyledPanel)
+
+        bottom = QFrame(self)
+        bottom.setFrameShape(QFrame.StyledPanel)
+
+        splitter1 = QSplitter(Qt.Horizontal)
+        splitter1.addWidget(topleft)
+        splitter1.addWidget(topright)
+
+        splitter2 = QSplitter(Qt.Vertical)
+        splitter2.addWidget(splitter1)
+        splitter2.addWidget(bottom)
+
+        hbox.addWidget(splitter2)
+        self.setLayout(hbox)
+
+        self.setGeometry(300, 300, 450, 400)
+        self.setWindowTitle('QSplitter')
+
+        self.layout= QVBoxLayout()
+
         #Switch active/stand by
-        def active():
-            print("active")
         self.b1=QPushButton("ACTIVE",self)
+        self.layout.addWidget(self.b1)
+        self.b2=QPushButton("Stand by",self)
+        self.layout.addWidget(self.b2)
+
+
+
         #self.b1.connect(active)
+        #act = QAction("Active", self)
+        #act.setStatusTip("This is your button")
+        #act.setCheckable(True)
 
 
         #Batterie
-        layout=QVBoxLayout()
+
         #Récuperer la charge de la battérie avec un connect maybe ? Voir Amine
 
-        self.bat=QProgressBar()
-        self.bat.setMinimum(0)
-        self.bat.setMaximum(100) #Le max de la charge
-        layout.addWidget(self.bat)
+        #self.bat=QProgressBar()
+        #self.bat.setMinimum(0)
+        #self.bat.setMaximum(100) #Le max de la charge
+        #layout.addWidget(self.bat)
 
 
+        #Jauges
         
 
 

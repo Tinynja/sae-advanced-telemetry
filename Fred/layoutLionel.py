c4991e5e70ddfe02bfbafde09161be4403c19a46
lioneloued17 <lioneloued17@gmail.com>
2021-03-06 15:58:54 -0500

    hey

--- Fred/layoutLionel.py
+++ Fred/layoutLionel.py
@@ -8,26 +8,26 @@ import sys
 
 class MainWindow(QMainWindow):
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super(MainWindow, self).__init__(*args, **kwargs)
 
-        #Switch active/stand by
-        #self.layout.addWidget(self.b1)
-        self.b1=QPushButton("ACTIVE",self)
-        self.b1.setGeometry(0,0,60,50)
-        # self.b1.clicked.connect()
+		#Switch active/stand by
+		
+		self.b1=QPushButton("ACTIVE",self)
+		self.b1.setGeometry(0,0,60,50)
+		# self.b1.clicked.connect()
 
-        #self.layout.addWidget(self.b2)
-        self.b2=QPushButton("Stand by",self)
-        self.b2.setGeometry(0,0,60,50)
-        self.b2.move(60,0)
-        # self.b2.clicked.connect()
-      
-
-
+		
+		self.b2=QPushButton("Stand by",self)
+		self.b2.setGeometry(0,0,60,50)
+		self.b2.move(60,0)
+		# self.b2.clicked.connect()
+	
 
 
-        #Batterie
+
+
+		#Batterie
 # class ProgressbarBat(QWidget):
 # 	def __init__(self):
 # 		super().__init__()
@@ -40,16 +40,16 @@ class MainWindow(QMainWindow):
 # 		self.Bat.setValue(self.step)
 
 
-        #Récuperer la charge de la battérie avec un connect maybe ? Voir Amine
+		#Récuperer la charge de la battérie avec un connect maybe ? Voir Amine
 
-        #self.bat=QProgressBar()
-        #self.bat.setMinimum(0)
-        #self.bat.setMaximum(100) #Le max de la charge
-        #layout.addWidget(self.bat)
+		#self.bat=QProgressBar()
+		#self.bat.setMinimum(0)
+		#self.bat.setMaximum(100) #Le max de la charge
+		#layout.addWidget(self.bat)
 
 
-        #Jauges
-        
+		#Jauges
+		
 
 
 
